7805f6c5abb755a9f9a177224706480c17c7c8d9
sakshamgoyal630 <goyalsaksham630@gmail.com>
2025-06-10 21:06:15 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"//treat all JS code as newer version
+// alert(3+3)//we are using nodejs,not browser
+console.log(3+3)
+console.log("saksham")
+let name="hitesh"
+let age=20
+let isLoggedIn=false
+//numebr ->2 to power 53
+//string->""
+//boolean
+//null
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+
