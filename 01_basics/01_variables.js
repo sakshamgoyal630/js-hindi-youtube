5a9a73a10cf28bc832bc383a8f3fd9386a8c6b61
sakshamgoyal630 <goyalsaksham630@gmail.com>
2025-06-10 20:54:03 +0000

    variable declaration

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail="goyalsaksham630@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+// accountId=2
+accountEmail="hello@gmail.com"
+accountCity="banglore"
+accountPassword="21212121"
+console.log(accountId);
+//prefered to use var
+//because of issue in block scope and functional scope
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
