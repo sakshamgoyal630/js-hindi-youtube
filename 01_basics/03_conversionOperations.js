2dafe4990d1c9ad0eac4d9d670751a0dc116a847
sakshamgoyal630 <goyalsaksham630@gmail.com>
2025-06-10 21:20:27 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,10 @@
+// let score ="33abc";
+// console.log(typeof score);
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+//"33"->33
+//"33abc"->NaN
+let isLoggedIn=1
+let booleanIsLoggedin=Boolean(isLoggedIn)
+console.log(booleanIsLoggedin)
